ce9c3be3d34a861b9119f38780023432d625cd45
Пользователь <world.worensi@gmail.com>
2023-11-19 16:17:21 +0600

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from './image/rectangle-39.png'
+
+const Card = ()=>{
+    return(
+      <div>
+          <img className={'imgCard'} src={Image} alt=""/>
+      </div>
+    )
+}
+export default Card
